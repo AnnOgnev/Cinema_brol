0bc722bfbd4fdde3e757de1da2178a9ed3abba80
Ogneva <ogneva_ttit@mail.ru>
2021-06-28 12:24:39 +0700

    Добавили замену базе данных и закончили мини-функционал

--- Cinema_brol/MainWindow.xaml.cs
+++ Cinema_brol/MainWindow.xaml.cs
@@ -20,154 +20,190 @@ namespace Cinema_brol
     /// </summary>
     public partial class MainWindow : Window
     {
+        Dictionary<int, string> database = new Dictionary<int, string>(32);
         public MainWindow()
         {
             InitializeComponent();
+            string name;
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(2, Name);
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(3, Name);
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(4, Name);
         }
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(5, Name);
         }
 
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(6, Name);
         }
 
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(7, Name);
         }
 
         private void Button_Click_7(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
 
         private void Button_Click_8(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
 
         private void Button_Click_9(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
 
         private void Button_Click_10(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
 
         private void Button_Click_11(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
 
         private void Button_Click_12(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_13(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_14(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_15(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_16(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_17(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_18(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_19(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_20(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_21(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_22(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_23(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_24(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_25(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_26(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_27(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_28(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_29(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_30(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_31(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
         private void Button_Click_32(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Для вас " + Name + " место забронировано!");
+            database.Add(1, Name);
         }
 
     }
---
+++ Cinema_brol/Name.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cinema_brol
+{
+    /// <summary>
+    /// Логика взаимодействия для Name.xaml
+    /// </summary>
+    public partial class Name : Window
+    {
+        public Name()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string UName = MyName.Text;
+            this.Hide();
+            MainWindow ds = new MainWindow();
+            ds.Name = UName;
+            ds.Show();
+        }
+    }
+}
