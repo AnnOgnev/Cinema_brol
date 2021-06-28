a27388fe737826db45350e47071b444dc8f712c0
Ogneva <ogneva_ttit@mail.ru>
2021-06-28 12:09:50 +0700

    кнопки бронирования

--- Cinema_brol/MainWindow.xaml.cs
+++ Cinema_brol/MainWindow.xaml.cs
@@ -24,5 +24,151 @@ namespace Cinema_brol
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_16(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_17(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_18(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_19(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_20(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_21(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_22(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_23(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_24(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_25(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_26(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_27(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_28(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_29(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_30(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_31(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_32(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
